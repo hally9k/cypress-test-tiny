7d6f6d91eb60f41bb681d344d1828c072271df09
hally9k <hally9k@gmail.com>
2018-07-09 14:07:25 +1200

    Add basic number input example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,17 @@
-describe('page', () => {
-  it('works', () => {})
+describe('Number Input', () => {
+	it('accepts typed input', () => {
+		cy.visit('http://localhost:8080')
+			.get('.number-input')
+			.type('2')
+			.should('have.value', '2')
+	})
+
+	it('accepts increment via the keyboard up button', () => {
+		cy.visit('http://localhost:8080')
+			.get('.number-input')
+			.type('2')
+			.should('have.value', '2')
+			.type('{uparrow}')
+			.should('have.value', '3')
+	})
 })
